f656c48c27b770ce794f5cc02674ee09565d371a
moral <moral@DESKTOP-TPSHLG8>
2016-10-16 13:26:04 -0400

    test commit

--- Midterm Coding Project/src/main/java/com/cisc181/core/Course.java
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Course.java
@@ -1,5 +1,17 @@
 package com.cisc181.core;
+import com.cisc181.eNums.eMajor;
+import java.util.UUID;
+
 
 public class Course {
-
+	private UUID CourseID;
+	private String CourseName;
+	private int GradePoints;
+	private eMajor Major;
+	
 }
+
+
+
+
+
